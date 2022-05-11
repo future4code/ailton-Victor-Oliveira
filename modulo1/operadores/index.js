d0eb7e3ffff4907bd67b7a4743572626115dee49
Victor santos oliveira <victorsantosoliveira198@gmail.com>
2022-05-11 20:41:20 -0300

    sem alteraçao

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,53 @@
+//Exercícios de interpretação de código
+
+//1-
+// a . false
+// b . false
+// c . false
+// d . boolean
+
+//2-
+//porque na verdadae ele está é concatenando e nao somando
+
+//3-
+//let primeiroNumero = Number (prompt("Digite um numero!"))
+//let segundoNumero = Number (prompt("Digite outro numero!"))
+
+//const soma = primeiroNumero + segundoNumero
+
+//console.log(soma)
+
+//----------------exercicio de escrita----------------------
+
+//const idade = prompt ("qual sua idade?")
+//const idadeAmigo = prompt (" qual idade do seu melhor amigo")
+
+//const  resultado = (idadeAmigo <= idade)
+
+//console.log("Sua idade é maior do que a do seu melhor amigo?", resultado)
+
+//console.log ("diferenca de idade de voces dois é ", idade - idadeAmigo )
+
+//2-a,b
+//const par = prompt ("insira um número par")
+
+//const resultado = par % 2
+//console.log(resultado) 
+
+//c,d-toda vez que é par da zero e toda vez que é impar da 1
+
+//3-
+//const anos = prompt ("qual sua idade")
+//const meses = anos * 12
+//const dias = meses * 30
+//const horas = dias * 24
+//console.log("voce tem" ,meses,"meses" ,dias, "dias", horas, "horas de vida")
+
+//4-
+//const valor1 = prompt("digite um número")
+//const valor2 = prompt("digite o segundo número")
+
+//const resultado = valor1 < valor2
+//const result = valor1 === valor2 
+//console.log("O primeiro numero é maior que segundo?" ,resultado)
+//console.log("O primeiro numero é igual ao segundo?", result)
