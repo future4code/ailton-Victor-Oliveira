ba35fa7d7fd5e259e32adea9576f17ecd26c2571
Victor santos oliveira <victorsantosoliveira198@gmail.com>
2022-05-17 18:44:15 -0300

    nada de alteraçao

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,53 @@
+//Exercícios de interpretação de código
+//1-a)o teste verifica se o número é multiplo de dois ou par ou impar ao mesmo tempo.
+// -b)números pares
+// -c)números impares
+
+//2-a)para o usuario saber o preço de uma fruta
+// -b)2.25
+// -c)o preço seria outro,pois a funçao nao iria para ali.
+
+//3-a)pedindo um número ao usúario
+// -b)seria "Esse número passou no teste",daria erro,pois o código le números apenas
+// -c)sim,
+
+//Exercícios de escrita de código
+//1-
+//let idade = Number(prompt("qual sua idade?"))
+//const permissao =(idade) =>{
+   // if (idade >= 18)
+    //return "Você pode dirigir"
+   // else {
+       // return "Você não pode dirigir."
+   // }
+//}  
+//console.log(permissao(idade))
+
+//2-
+
+
+
+
+
+//3-
+//let escola = prompt("Qual seu turno?")
+
+// switch(escola.toLowerCase()) {
+   //  case 'matutino': 
+     //    console.log("bom dia")
+   //  break
+    // case 'vespertino':
+   //      console.log("boa tarde")
+    // break
+  //   case 'noturno': 
+   //      console.log('boa noite')
+   //  break
+// }
+
+//4-
+
+
+
+
+
+
