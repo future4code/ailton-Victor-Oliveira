bc4f1101565775eea00f2882fbbf785020186c9a
Victor santos oliveira <victorsantosoliveira198@gmail.com>
2022-05-19 18:10:52 -0300

    funcoes de array

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,12 @@
+//Exercícios de interpretação de código
+//1-
+
+
+//Exercícios de escrita de código
+//1-
+const numeros = [1, 2, 5, 8, 10, 11, 13, 15, 20]
+ const retornaNumerospares = numeros.filter((numeros) =>{
+     
+     return numeros === 1
+ })
+ console.log(retornaNumerospares)
